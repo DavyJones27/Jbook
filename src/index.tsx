f30706d8a19cd161f16cd9ba186e2d378e1f53cd
DavyJones27 <ajayyadavkol16@gmail.com>
2021-07-19 16:20:56 +0530

    esbuild set up

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,53 @@
+import * as esbuild from 'esbuild-wasm';
+import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
+import { unpkgPathPlugin } from './plugin/unpkg-path-plugin';
 
 
 const App = () => {
-    return <h1>Hi there!!!</h1>
+    const ref = useRef<any>();
+    const [input, setInput] = useState<string>("");
+    const [code, setCode] = useState<string>("");
+
+    const startService = async () => {
+        ref.current = await esbuild.startService({
+            worker: true,
+            wasmURL: '/esbuild.wasm'
+        });
+    }
+
+    useEffect(() => {
+        startService()
+    }, [])
+
+    const onClick = async () => {
+        if (!ref.current) {
+            return;
+        }
+
+        const result = await ref.current.build({
+           entryPoints: ['index.js'],
+            bundle: true,
+            write: false,
+            plugins: [unpkgPathPlugin()]
+        }); 
+        console.log(result);
+        
+        setCode(result.outputFiles[0].text);
+    }
+
+
+    return <div>
+        <textarea onChange={e => setInput(e.target.value)} value={input} />
+        <div>
+            <button onClick={onClick}>
+                Submit
+            </button>
+        </div>
+        <pre>
+            {code}
+        </pre>
+    </div>
 }
 
 ReactDOM.render(
